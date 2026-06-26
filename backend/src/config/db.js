da2342b4baccbfed996558308988ebbd0f05ec6b
SUMITKASULA <107529051+SUMITKASULA@users.noreply.github.com>
2026-06-26 22:25:18 +0545

    add MongoDb connection

---
+++ backend/src/config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connectDB = async (mongoUri) => {
+  try {
+    await mongoose.connect(mongoUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection failed:', error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
